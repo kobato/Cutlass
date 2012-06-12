af7cf5bfa9f54081b972116c5e7623812bd9790f
Aria Furukawa <top@scientist.com>
2012-06-12 18:36:32 +0800

    Not .net

--- admin/views/footer.php
+++
@@ -1,9 +0,0 @@
-<?php if(!defined('EMLOG_ROOT')) {exit('error!');}?>
-</td></tr></tbody></table>
-<div>
-<p align="center">欢迎使用 &copy; <a href="http://www.emlog.net" target="_blank">emlog</a></p>
-<?php doAction('adm_footer');?>
-</div>
-</div>
-</body>
-</html>
